bbe978aaa256fdffb631aee4ed80317af55c1fd9
moorejr5 <james.r.moore@vanderbilt.edu>
2019-04-22 16:10:20 -0500

    Switched 'saveData' to use json instead of array.

--- AutoRecordGenerationExternalModule.php
+++ AutoRecordGenerationExternalModule.php
@@ -97,11 +97,6 @@ class AutoRecordGenerationExternalModule extends AbstractExternalModule
 			}
 
 			//$this->saveData($targetProjectID,$dataToPipe[$targetProject->table_pk],$targetProject->firstEventId,$dataToPipe);
-            echo "Target Project: $targetProjectID, Table PK: ".$targetProject->table_pk.", Target Event: ".$targetProject->firstEventId.", Overwrite: ".$overwrite."<br/>";
-			echo "Data to pipe:<br/>";
-			echo "<pre>";
-			print_r($dataToPipe);
-			echo "</pre>";
             //$results = \Records::saveData($targetProjectID, 'array', [$dataToPipe[$targetProject->table_pk] => [$targetProject->firstEventId => $dataToPipe]],$overwrite);
             $results = \Records::saveData($targetProjectID, 'json', json_encode(array($dataToPipe)),$overwrite);
 
