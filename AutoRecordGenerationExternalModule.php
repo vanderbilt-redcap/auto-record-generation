3acb67cfd9b962f0c88c7efa3a003d736551fe24
moorejr5 <james.r.moore@vumc.org>
2026-05-22 11:19:16 -0500

    Bug fix for incorrectly catching matching repeat instances.

--- AutoRecordGenerationExternalModule.php
+++ AutoRecordGenerationExternalModule.php
@@ -101,7 +101,7 @@ class AutoRecordGenerationExternalModule extends AbstractExternalModule
 			foreach ($results as $indexData) {
 				if (
 					(!isset($indexData['redcap_event_name']) || $indexData['redcap_event_name'] == $eventName) &&
-					(!isset($indexData['redcap_repeat_instance']) || $indexData['redcap_repeat_instance'] == $repeat_instance) &&
+					(!isset($indexData['redcap_repeat_instance']) || ($indexData['redcap_repeat_instance'] ?: "1") == $repeat_instance) &&
 					$indexData[$flagFieldName] != ""
 				) {
 					$triggerFieldValue = $indexData[$flagFieldName];
